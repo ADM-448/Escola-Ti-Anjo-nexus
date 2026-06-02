472c2d0efbafaa0c8597b6a77f37a07cb9aa55f3
Gabriel S. Guerra <159430864+gabriel-guerra@users.noreply.github.com>
2026-06-01 21:58:45 -0300

    Alteração na API para ativar os endpoints dos editais

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\JogoController;
+// use App\Http\Controllers\JogoController;
+use App\Http\Controllers\EditalController;
 
 // Rota de Login (pública)
 Route::post('/login', [AuthController::class, 'login']);
 
 // Rotas públicas de jogos
-Route::get('/jogos', [JogoController::class, 'index']);
-Route::get('/jogos/{id}', [JogoController::class, 'show']);
-Route::post('/jogos', [JogoController::class, 'store']);
-Route::put('/jogos/{id}', [JogoController::class, 'update']);
-Route::delete('/jogos/{id}', [JogoController::class, 'destroy']);
+// Route::get('/jogos', [JogoController::class, 'index']);
+// Route::get('/jogos/{id}', [JogoController::class, 'show']);
+// Route::post('/jogos', [JogoController::class, 'store']);
+// Route::put('/jogos/{id}', [JogoController::class, 'update']);
+// Route::delete('/jogos/{id}', [JogoController::class, 'destroy']);
+
+Route::get('/editais', [EditalController::class, 'index']);
+Route::get('/editais/{id}', [EditalController::class, 'show']);
